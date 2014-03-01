2a8424d3a5bf4066d3fba6f10817d80d070456f6
Marcus Ahnve <marcus@ahnve.com>
2014-03-01 21:59:06 +0100

    Add GA

--- src/skidskytte/views.clj
+++ src/skidskytte/views.clj
@@ -15,6 +15,17 @@
 (def tweet-button "<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-url=\"http://skidskytteroulette.se\" data-text=\"Hitta din nya sport på \" data-lang=\"sv\">Tweeta</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>")
 
+(def google-analytics "<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-160245-6', 'skidskytteroulette.se');
+  ga('send', 'pageview');
+
+</script>")
+
 (defn main [name]
   (hp/html5
     [:head
@@ -29,5 +40,6 @@
         [:p.something-else "Nja, det låter trist, jag vill pröva "
          [:a {:href "/"} "något annat"]]
         [:div.fb-like fb-like-button]
-        [:div.tweet-button tweet-button]]]]))
+        [:div.tweet-button tweet-button]]]
+     [:div google-analytics]]))
 
