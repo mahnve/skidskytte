4156f819c25da16c210fae613fd8636c24c8da5b
Marcus Ahnve <m@hnve.org>
2021-01-31 19:43:21 +0100

    Better turbo link

--- src/clj/skidskytte/views.clj
+++ src/clj/skidskytte/views.clj
@@ -29,7 +29,7 @@
     (hp/include-css "/assets/font-awesome/css/font-awesome.min.css")
     (hp/include-css "/css/screen.css")
     (hp/include-css "/css/custom.css")
-    (hp/include-js "https://cdn.skypack.dev/pin/@hotwired/turbo@v7.0.0-beta.4-TQFv5Y2xd4hn2VnTxVul/min/@hotwired/turbo.js")
+    (hp/include-js "https://unpkg.com/@hotwired/turbo@7.0.0-beta.3/dist/turbo.es5-umd.js")
     matomo]
    [:body
     [:div.container
